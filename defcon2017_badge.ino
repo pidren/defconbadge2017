ec18940c4d3410467ef101c7ed85ace9e9907f77
ren <ren@Jack.gateway.sonic.net>
2017-07-11 21:31:54 -0700

    Lots of library functions added 1. RGB color channel controls, rainbow function 2. LED controls, flicker 3. Starting loading/banner done 4. Wrote notes in sections where Wifi Server setup should go.

--- defcon2017_badge.ino
+++ defcon2017_badge.ino
@@ -9,7 +9,11 @@ Adafruit_SSD1306 display(OLED_RESET);
 
 #define LOGO16_GLCD_HEIGHT 16 
 #define LOGO16_GLCD_WIDTH  16 
-//XXX: Need to change this to some logo that is useful.
+
+//--------------------------------------------------------------------------------
+// ------------------------------ BITMAP LOGO ------------------------------------
+//--------------------------------------------------------------------------------
+//*** TODO: Need to change this to some logo that is useful.
 static const unsigned char PROGMEM logo16_glcd_bmp[] =
 { B00000000, B00000000,
   B00000001, B10000000,
@@ -28,12 +32,13 @@ static const unsigned char PROGMEM logo16_glcd_bmp[] =
   B00000000, B00000000,
   B00000000, B00000000 };
 
-
-// ------ PIN MAPPINGS --------
+//--------------------------------------------------------------------------------
+// ------------------------------ PIN MAPPINGS -----------------------------------
+//--------------------------------------------------------------------------------
 #define BUILTIN_LED 16 // G of BOTTOM_MID_RGB
 #define D0  16         // G of BOTTOM_MID_RGB
-#define D1  5          // ?? -- screen?
-#define D2  4          // ?? -- screen?
+#define D1  5          // screen - DO NOT USE!!! Already handled by library.
+#define D2  4          // screen - DO NOT USE!!! Already handled by library.
 #define D3  0          // B of BOTTOM_MID_RGB
 #define D4  2          // R of BOTTOM_MID_RGB, 
 #define D5  14         // G of BOTTOM_RIGHT_RGB
@@ -45,100 +50,148 @@ static const unsigned char PROGMEM logo16_glcd_bmp[] =
 #define TL  10         // TOPLEFT LED -- this is also SDD3
 #define TR  9          // TOPRIGHT LED -- tihs is also SDD2
 
-uint8_t all_pins[] = {D0,D3,D4,D5,D6,D7,D8,D9,D10,TL,TR};
-uint8_t bottom_mid_rgb[] = {D4,D0,D3}; // R, G, B in indexing order
-uint8_t bottom_right_rgb[] = {D7,D5,D6};
-uint8_t bottom_left_rgb[] = {D10,D8,D9};
+//RGB grouping per RGB LED in R, G, B order
+uint8_t BOT_MD_RGB[] = {D4,D0,D3};
+uint8_t BOT_RT_RGB[] = {D7,D5,D6};
+uint8_t BOT_LT_RGB[] = {D10,D8,D9};
+
+//R, G, B constants for easy access.
+#define R 0
+#define G 1
+#define B 2
 
 
-
-void showText(void) {
+//--------------------------------------------------------------------------------
+//--------------------------------- TEXT HELPERS ---------------------------------
+//--------------------------------------------------------------------------------
+void setBanner(void){
+  // *** TODO: Want to use logo16_glcd_bmp for bitmap logo here ***
   display.setTextSize(2);
   display.setTextColor(WHITE);
-  display.setCursor(10,0);
-  display.clearDisplay();
-  display.println("YAY!!!");  
-  display.print("DEFCON 25 BADGE");
-  display.display();
-  delay(1);
+  display.setCursor(12,0);
+  display.print("DEFCON 25");
+  display.setCursor(12,20);
+  display.print("<< MRB >>");
+  display.setCursor(28,55);
+  display.setTextSize(1);
+  display.print("CUSTOM BADGE");
 }
 
+void setLoading(void){
+  display.setTextSize(3);
+  display.setTextColor(WHITE);
+  display.setCursor(0,25);
+  display.println("Loading...");
+}
+
+void setupRGBs(void){
+  setRGB(BOT_MD_RGB, 0, 0, 0);
+  setRGB(BOT_LT_RGB, 0, 0, 0);
+  setRGB(BOT_RT_RGB, 0, 0, 0);
+}
+
+// *** TODO: TEST SCROLL in 3 LANES
+
+
+//--------------------------------------------------------------------------------
+//---------------------------- ANIMATION HELPERS ---------------------------------
+//--------------------------------------------------------------------------------
+void setRGB(uint8_t* rgb, uint8_t r_chan, uint8_t g_chan, uint8_t b_chan){
+  //Set RGB channels for the RGB LEDs
+  analogWrite(rgb[R], r_chan);
+  analogWrite(rgb[G], g_chan);
+  analogWrite(rgb[B], b_chan);
+}
+
+void setLED(uint8_t led, uint8_t v){
+  analogWrite(led, v);
+}
+
+void rainbowRGB(uint8_t* rgb){
+  //Tries to simulate rainbow for all RGB LEDs
+  //Red(255,0,0) --> Green(0,255,0) --> Blue(0,0,255)
+  setRGB(rgb,255,0,0);
+  delay(300);
+  for(int i=255; i>=0; i--){
+    setRGB(rgb, i, abs(i-255), 0);
+    delay(2);
+  }
+  for(int i=255; i>=0; i--){
+    setRGB(rgb,0, i, abs(i-255));
+    delay(2); 
+  }    
+}
+
+void flickerLED(uint8_t led){
+  //Quick flicker animation for LED, best used for visual status
+  digitalWrite(led, HIGH);
+  delay(10);
+  digitalWrite(led, LOW);
+}
+
+//--------------------------------------------------------------------------------
+//---------------------------- WIFI SERVER HELPERS -------------------------------
+//--------------------------------------------------------------------------------
+//*** TODO: listen to packet and synchronously scroll text
+
+
+
+//--------------------------------------------------------------------------------
+//----------------------------------- SETUP --------------------------------------
+//--------------------------------------------------------------------------------
 void setup()   {  
-  //Set baud rate to 115200
+  // Set baud rate to 115200 for Serial Logs
   Serial.begin(115200);
+  Serial.print("Starting badge");
   
   // Generate the high voltage from the 3.3v line internally
   // Initialize with the I2C addr 0x3D (for the 128x64)
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  
-  
-  // Show image buffer on the display hardware.
-  // Since the buffer is intialized with an Adafruit splashscreen
-  // internally, this will display the splashscreen.
-  display.display();
-  delay(2000);
 
-  // Clear the buffer.
-  display.clearDisplay(); 
-
-  Serial.println( "Initializing all pins to OUTPUT mode." );
-  
-  pinMode(D0,OUTPUT);
-  pinMode(D3,OUTPUT);
-  pinMode(D4,OUTPUT);
-  pinMode(D5,OUTPUT);
-  pinMode(D6,OUTPUT);
-  pinMode(D7,OUTPUT);
-  pinMode(D8,OUTPUT);
-  pinMode(D9,OUTPUT);
-  pinMode(D10,OUTPUT);
+  // Initialize Pins (note: putting this in a loop does not work.)
+  pinMode(BOT_MD_RGB[R],OUTPUT);
+  pinMode(BOT_MD_RGB[G],OUTPUT);
+  pinMode(BOT_MD_RGB[B],OUTPUT);
+  pinMode(BOT_LT_RGB[R],OUTPUT);
+  pinMode(BOT_LT_RGB[G],OUTPUT);
+  pinMode(BOT_LT_RGB[B],OUTPUT);
+  pinMode(BOT_RT_RGB[R],OUTPUT);
+  pinMode(BOT_RT_RGB[G],OUTPUT);
+  pinMode(BOT_RT_RGB[B],OUTPUT);
   pinMode(TL,OUTPUT);
   pinMode(TR,OUTPUT);
   
-  /*
-  // XXX: For some reason this code does not work.
-  for(int i=0; sizeof(all_pins); i++){
-    pinMode(all_pins[i],OUTPUT); 
-    Serial.println(all_pins[i]);
-    delay(600);
-  }
-  Serial.println("Done.");
-  */
+  //Initialize RGB LED
+  setupRGBs();
   
-  showText();
+  // Show loading screen.
+  display.clearDisplay(); 
+  setLoading();
+  display.display();
+  // *** TODO: Initialize WiFi Server Here ***
+  delay(2000);
+  
+  // Show Default Banner
+  display.clearDisplay();
+  setBanner();
+  display.display();
+  delay(100);
 }
   
 
+//--------------------------------------------------------------------------------
+//--------------------------------- MAIN LOOP ------------------------------------
+//--------------------------------------------------------------------------------
 void loop() {
-  //As a test turn on R,G,B until all white
-  //As a test turn on/off top LEDs
-  digitalWrite(D4,HIGH);
-  digitalWrite(D7,HIGH);
-  digitalWrite(D10,HIGH);
-  digitalWrite(TL,HIGH);
-  digitalWrite(TR,HIGH);
-  delay(500);
-  digitalWrite(D0,HIGH);
-  digitalWrite(D5,HIGH);
-  digitalWrite(D8,HIGH);
-  digitalWrite(TL,LOW);
-  digitalWrite(TR,LOW);
-  delay(500);
-  digitalWrite(D3,HIGH);
-  digitalWrite(D6,HIGH);
-  digitalWrite(D9,HIGH);
-  digitalWrite(TL,HIGH);
-  digitalWrite(TR,HIGH);
-  delay(500);
-  digitalWrite(D0,LOW);
-  digitalWrite(D3,LOW);
-  digitalWrite(D4,LOW);
-  digitalWrite(D5,LOW);
-  digitalWrite(D6,LOW);
-  digitalWrite(D7,LOW);
-  digitalWrite(D8,LOW);
-  digitalWrite(D9,LOW);
-  digitalWrite(D10,LOW);
-  digitalWrite(TL,LOW);
-  digitalWrite(TR,LOW);
-  delay(500);
+  rainbowRGB(BOT_MD_RGB);
+  flickerLED(TL);
+  //*** TODO: LISTEN FOR PACKET ***
+  rainbowRGB(BOT_LT_RGB);
+  flickerLED(TR);
+  //*** TODO: LISTEN FOR PACKET ***
+  rainbowRGB(BOT_RT_RGB);
+  flickerLED(TL);
+  flickerLED(TR);
+  //*** TODO: LISTEN FOR PACKET ***
+  //*** May want to unroll rainbowRGB and listen in between setRGB calls
 }
